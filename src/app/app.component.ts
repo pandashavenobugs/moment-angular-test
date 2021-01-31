8dd9b04145d83661d54130569f340a2818ddfe9d
berat <cedinckan@gmail.com>
2021-01-31 20:46:00 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as moment from "moment";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  date:any;
   title = 'momentTest';
+  constructor(){
+    const now = new Date().getTime()
+    moment.locale('tr')
+    this.date = moment(now).format('llll');
+
+  }
 }
